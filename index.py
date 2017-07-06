53b0e458343dd4bce9fb83b39a98bbfbc6f02d63
Fabian Zeiher <fzeiher@gmail.com>
2017-07-06 09:48:43 +0200

    FEAT: display sample piechart

--- index.py
+++ index.py
@@ -2,3 +2,4 @@ import api
 import visual
 
 visual.barplot()
+visual.piechart()
--- visual.py
+++ visual.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 SAMPLE_LABELS = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So']
 SAMPLE_DATA = [1,2,3,4,5,6,7]
+SAMPLE_LABELS_PIE = ['Norway', 'Sweden', 'Great Britain', 'France',  'Japan']
+SAMPLE_DATA_PIE = [1000, 750, 500, 350, 100]
 
 
 def barplot(labels=SAMPLE_LABELS,data=SAMPLE_DATA):
@@ -16,4 +18,13 @@ def barplot(labels=SAMPLE_LABELS,data=SAMPLE_DATA):
     ax.bar(pos,data)
     ax.set_ylabel('Hashtaganzahl')
     ax.set_title('Hashtag Performance')
+
+    plt.show()
+
+def piechart(labels=SAMPLE_LABELS_PIE,data=SAMPLE_DATA_PIE):
+    fig1, ax1 = plt.subplots()
+
+    ax1.pie(data, labels=labels, autopct='%1.2f%%', shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
     plt.show()
