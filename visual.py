05913ac7afd9d68b9de8c16b3ea81c94f564d819
Fabian Zeiher <fzeiher@gmail.com>
2017-07-08 16:02:13 +0200

    FEAT: display all plots in one window FIX: correct titles and sample data on all dpots REFACTOR: display barplot horizontal

--- visual.py
+++ visual.py
@@ -3,9 +3,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import json
 
-SAMPLE_LABELS = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So']
+SAMPLE_LABELS = ['The', 'Donald', 'is', 'a', 'f******', 'disgusting', 'Potato']
 SAMPLE_DATA = [1,2,3,4,5,6,7]
 SAMPLE_LABELS_PIE = ['Norway', 'Sweden', 'Great Britain', 'France',  'Japan']
 SAMPLE_DATA_PIE = [1000, 750, 500, 350, 100]
@@ -14,59 +13,62 @@ SAMPLE_DATA_SCATTER = np.random.randn(24*7-1)
 SAMPLE_GEO_DATA = np.asarray([[12,125],[89,105],[27,110],[24,113]])
 
 class Visual(object):
+    def __init__(self):
+        fig = plt.figure(figsize=(13,5))
+
+    def display(self):
+        plt.tight_layout()
+        plt.show()
+
     def barplot(self, labels=SAMPLE_LABELS,data=SAMPLE_DATA):
-        fig, ax = plt.subplots()
+        ax1 = plt.subplot2grid((2,6), (0,0), rowspan=1)
 
         pos=np.arange(len(data))
-        ax.set_xticks(pos)
-        ax.set_xticklabels(labels)
-        ax.bar(pos,data)
-        ax.set_ylabel('Hashtaganzahl')
-        ax.set_title('Hashtag Performance')
+        ax1.set_yticks(pos)
+        ax1.set_yticklabels(labels)
+        ax1.invert_yaxis()
 
-        plt.show()
+        ax1.set_xlabel('Hashtag Count')
+        ax1.set_title('Cluster')
+        ax1.barh(pos,data)
 
     def piechart(self, labels=SAMPLE_LABELS_PIE,data=SAMPLE_DATA_PIE):
-        fig1, ax1 = plt.subplots()
+        ax2 = plt.subplot2grid((2,6), (0,1), colspan=2)
 
-        ax1.pie(data, labels=labels, autopct='%1.2f%%', shadow=True, startangle=90)
-        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-        plt.show()
+        ax2.pie(data, labels=labels, autopct='%1.2f%%', shadow=True, startangle=55)
+        ax2.set_title('Origin')
+        ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
     def scatterplot(self, labels=SAMPLE_LABELS_SCATTER, data=SAMPLE_DATA_SCATTER):
-        fig, ax = plt.subplots()
+        ax3 = plt.subplot2grid((2,6), (1,0), colspan=3)
 
         days = np.arange(len(labels))
-        #ax.set_xticks(days)
-        #ax.set_xticklabels(labels)
-        ax.scatter(days,data)
-
-        plt.show()
+        ax3.set_ylabel('Hashtag Count')
+        ax3.set_title('Performance')
+        ax3.scatter(days,data)
 
     def globalscatter(self, geoData=SAMPLE_GEO_DATA):
         loadList = ['global_boundries.csv', 'australien.csv']
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
+        ax4 = plt.subplot2grid((2,6), (0,3), colspan=3, rowspan=3, projection='3d')
 
         for i in range(len(loadList)):
             data = np.genfromtxt(loadList[i], delimiter=',')
             x_array, y_array, z_array = dataconverter(data)
 
-            ax.scatter(x_array, y_array, z_array, c='b', marker='.')
-            ax.plot(x_array, y_array, z_array, c='b')
+            ax4.scatter(x_array, y_array, z_array, c='b', marker='.')
+            ax4.plot(x_array, y_array, z_array, c='b')
 
         x_array, y_array, z_array = dataconverter(geoData)
-        ax.scatter(x_array, y_array, z_array, c='r', marker='^')
+        ax4.scatter(x_array, y_array, z_array, c='r', marker='^')
 
-        ax.set_aspect('equal')
-        ax.axis('off')
-        plt.show()
+        ax4.set_title('Geo Data')
+        ax4.set_aspect('equal')
+        ax4.axis('off')
 
 def dataconverter(data):
-    an = np.cos(data[:,0]*(np.pi/180))*1
+    an = np.cos(data[:,0]*(np.pi/180))
     x_array = np.cos(data[:,1]*(np.pi/180))*an
     y_array = np.sin(data[:,1]*(np.pi/180))*an
-    z_array = np.sin(data[:,0]*(np.pi/180))*1
+    z_array = np.sin(data[:,0]*(np.pi/180))
     return x_array, y_array, z_array
