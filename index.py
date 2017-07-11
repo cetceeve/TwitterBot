35ae0b0a67c4dccb4fc98d35e70e3f272dd6c520
Felix Wieland <felix@buntstift-media.com>
2017-07-11 16:44:08 +0200

    brought back from presentation mode

--- index.py
+++ index.py
@@ -5,8 +5,8 @@ link, token = twitter.getAuthLink()
 pin = input(link + ' : ')
 if twitter.getToken(pin, token):
     print 'Your name is: ' + twitter.api.me().screen_name
+    print twitter.api.me().followers_count
 
-
-visual.barplot()
-visual.piechart()
-visual.scatterplot()
+ visual.barplot()
+ visual.piechart()
+ visual.scatterplot()
