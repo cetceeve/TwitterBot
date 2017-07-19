252b635246c924e04563673e7deb2dde9a0a1d9e
Fabian Zeiher <fzeiher@gmail.com>
2017-07-19 09:28:33 +0200

    CLEANUP: installed a linter for python, forced me to do this cleanup

--- index.py
+++ index.py
@@ -8,80 +8,82 @@ from collections import OrderedDict
 
 # ------------  Need a LOT of cleaning  ------------- #
 
-#TODO: only add the ~5 most used hashtags (maybe use sets for increased speed)
-#returns updated HashtagDic using content of one tweet
+
+# TODO: only add the ~5 most used hashtags (maybe use sets for increased speed)
+# returns updated HashtagDic using content of one tweet
 def getHashtags(searchStr, dic):
-    #regEx search for hastags in Tweets
-    #'hashtags' = list of found hashtags
-    hashtags =  re.findall('#\S+', searchStr)
-    #iterate through found hashtags
+    # regEx search for hastags in Tweets
+    # 'hashtags' = list of found hashtags
+    hashtags = re.findall('#\S+', searchStr)
+    # iterate through found hashtags
     for pos in hashtags:
         if pos in dic:
-            dic.update({pos : dic[pos]+1})
+            dic.update({pos: dic[pos] + 1})
         else:
-            dic.update({pos :1})
+            dic.update({pos: 1})
     return dic
 
-#same as above (without regEx)
+
+# same as above (without regEx)
 def getCountry(country, countDic):
     if country in countDic:
-        countDic.update({country : countDic[country]+1})
+        countDic.update({country: countDic[country] + 1})
     else:
-        countDic.update({country : 1})
+        countDic.update({country: 1})
     return countDic
 
-#same as above
-def timestamp(searchStr,Hourdic):
-    #find current day/hour using regEx
+
+# same as above
+def timestamp(searchStr, Hourdic):
+    # find current day/hour using regEx
     day = str(re.findall('^\w{2,3}', searchStr))[2:-2]
     hour = int(str(re.findall('\s\d{2}[:]', searchStr))[2:-3])
 
-    #add +1 tweet to found hour of found day
-    Hourdic[day][hour] = Hourdic[day][hour]+1
+    # add +1 tweet to found hour of found day
+    Hourdic[day][hour] = Hourdic[day][hour] + 1
 
     return Hourdic
 
 
+# API
+# link, token = twitter.getAuthLink()
+# pin = input(link + ' : ')
+# if twitter.getToken(pin, token):
+    # tweets = twitter.getTweetsByHashtag('nog20p')
 
-#API
-#link, token = twitter.getAuthLink()
-#pin = input(link + ' : ')
-#if twitter.getToken(pin, token):
-    #tweets = twitter.getTweetsByHashtag('nog20p')
-
-#####TESTTWEETS  Delete later
+# ####TESTTWEETS  Delete later
 tweets = [{"text": "#abc #def lol", "username": "test", "timestamp": "Thu Jul 06 19:37:36 +0000 2017", "geo": "de"},
-{"text": "#abcd #abc", "username": "testtest", "timestamp": "Thu Jul 06 20:37:36 +0000 2017", "geo": "gb"}]
+          {"text": "#abcd #abc", "username": "testtest", "timestamp": "Thu Jul 06 20:37:36 +0000 2017", "geo": "gb"}]
 ################
 
-#create empty dicts to be filled later
-#should dicts be to slow use sets
+# create empty dicts to be filled later
+# should dicts be to slow use sets
 HashtagDic = {}
 CountryDic = {}
-#ordered Dic (Weekday order is important)
+# ordered Dic (Weekday order is important)
 TimeDic = OrderedDict()
-#creating weekdays and filling every hour with '0' tweets
-#(to be edited later)
-for x in ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]:
-    TimeDic.update({x : {0 : 0}})
-    for y in range(0,24):
+# creating weekdays and filling every hour with '0' tweets
+# (to be edited later)
+for x in ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]:
+    TimeDic.update({x: {0: 0}})
+    for y in range(0, 24):
         TimeDic[x][y] = 0
 
-#iterate through every tweetList
+# iterate through every tweetList
 for tweet in tweets:
     for key in tweet:
         if key == "text":
-            #creates a dic: 'HashtagName' : 'HashtagCount'
-            HashtagDic = getHashtags(tweet["text"],HashtagDic)
+            # creates a dic: 'HashtagName' : 'HashtagCount'
+            HashtagDic = getHashtags(tweet["text"], HashtagDic)
         if key == "geo":
-            #creates a dic 'Countries' : 'countryCount'
+            # creates a dic 'Countries' : 'countryCount'
             CountryDic = getCountry(tweet["geo"], CountryDic)
         if key == "timestamp":
-            #create a dict in a dict:
-            #Weekdays as keys : (Hour of the day : tweetCount)
+            # create a dict in a dict:
+            # Weekdays as keys : (Hour of the day : tweetCount)
             TimeDic = timestamp(tweet["timestamp"], TimeDic)
 
-#write every Dict in a npArray to give to visual
+# write every Dict in a npArray to give to visual
 Hashtags = np.asarray(HashtagDic.keys())
 HashtagNumbers = np.asarray(HashtagDic.values())
 
@@ -89,46 +91,45 @@ Countries = np.asarray(CountryDic.keys())
 countryCount = np.asarray(CountryDic.values())
 
 Days = np.asarray(TimeDic.keys())
-#create a empty list to write the tweetCounts of every hour of every day in order into ONE List
-#then create a npArray of the list
+# create a empty list to write the tweetCounts of every hour of every day in order into ONE List
+# then create a npArray of the list
 Hours = []
 for key in TimeDic:
     Hours.append(TimeDic[key].values())
 Hourlist = np.asarray(sum(Hours, []))
 
-#Visualisierung
-#Instatiert ein Visual-Object, Initiert das Fenster
-#kein input
+# Visualisierung
+# Instatiert ein Visual-Object, Initiert das Fenster
+# kein input
 vis = visual.Visual()
 
 # Plot zeigt andere Hashtags die oft zusammen mit dem gesuchten Hashtag geschrieben wurden
-#Für-einen-Datenpunkt:
+# Für-einen-Datenpunkt:
 # auf Platz1 stehen die Hashtags
 # auf Platz2 steht die Zahl, wie oft ein Hashtag zusammen mit dem gesuchten Hashtag gefunden wurde
-#Input: Platz1: List of Strings, Platz2: List of Integers
-vis.barplot(Hashtags,HashtagNumbers)
+# Input: Platz1: List of Strings, Platz2: List of Integers
+vis.barplot(Hashtags, HashtagNumbers)
 
 # Plot zeigt aus welchem Land die Tweets mit dem gesuchten Hashtag kommen
-#Für-einen-Datenpunkt:
+# Für-einen-Datenpunkt:
 # auf Platz1 stehen die Namen der Länder
 # auf Platz2 steht die Zahl der Tweets aus dem zugehörigen Land
-#Input: Platz1: List of Strings, Platz2: List of Integers
-vis.piechart(Countries,countryCount)
+# Input: Platz1: List of Strings, Platz2: List of Integers
+vis.piechart(Countries, countryCount)
 
 # Plot zeigt die Gesamtzahl von Tweets im zeitlichen Verlauf der letzten 7 Tage
-#Für-einen-Datenpunkt:
+# Für-einen-Datenpunkt:
 # auf Platz1 stehen die Wochentage mit zwei Buchstaben also 'Mo', 'Di', usw.
 # auf Platz2 steht die Anzahl der Tweets innerhalb einer Stunde!
-#Input: Platz1: List of Strings, Platz2: List of Integers
+# Input: Platz1: List of Strings, Platz2: List of Integers
 vis.scatterplot(Days, Hourlist)
 
 # Plot zeigt die Welt und die genaue Position der Tweets in 3D
 # Als Dataset werden etwa 1000 Koordinatenpunkte <<[Breitengrad, Längengrad]>> benötigt
-#Input: 2D Array of floats <<np.asarray([[0,0],[0,180]])>>
-#vis.globalscatter()
+# Input: 2D Array of floats <<np.asarray([[0,0],[0,180]])>>
+# vis.globalscatter()
 
-#Zeigt das Fenster mit allen Plots
-#!wartet bis das Plotfenster geschlossen wird!
-#kein input
+# Zeigt das Fenster mit allen Plots
+# !wartet bis das Plotfenster geschlossen wird!
+# kein input
 vis.display()
-
--- visual.py
+++ visual.py
@@ -6,42 +6,43 @@ import matplotlib.ticker as ticker
 from mpl_toolkits.mplot3d import Axes3D
 
 SAMPLE_LABELS = np.asarray(['hashtag_1', 'hashtag_2', 'hashtag_3', 'hashtag_4', 'hashtag_5'])
-SAMPLE_DATA = np.asarray([10,50,125,250,500])
-SAMPLE_LABELS_PIE = np.asarray(['Norway', 'Sweden', 'Great Britain', 'France',  'Japan'])
+SAMPLE_DATA = np.asarray([10, 50, 125, 250, 500])
+SAMPLE_LABELS_PIE = np.asarray(['Norway', 'Sweden', 'Great Britain', 'France', 'Japan'])
 SAMPLE_DATA_PIE = np.asarray([1000, 750, 500, 350, 200])
 SAMPLE_LABELS_SCATTER = np.asarray(['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'])
-SAMPLE_DATA_SCATTER = np.random.randn(24*7)
-SAMPLE_GEO_DATA = np.asarray([[0,0],[90,0],[-90,0],[0,180],[0,90],[0,-90]])
+SAMPLE_DATA_SCATTER = np.random.randn(24 * 7)
+SAMPLE_GEO_DATA = np.asarray([[0, 0], [90, 0], [-90, 0], [0, 180], [0, 90], [0, -90]])
+
 
 class Visual(object):
     def __init__(self):
-        fig = plt.figure(figsize=(13,5))
+        fig = plt.figure(figsize=(13, 5))
 
     def display(self):
         plt.tight_layout()
         plt.show()
 
-    def barplot(self, labels=SAMPLE_LABELS,data=SAMPLE_DATA):
-        ax1 = plt.subplot2grid((2,6), (0,0), rowspan=1)
+    def barplot(self, labels=SAMPLE_LABELS, data=SAMPLE_DATA):
+        ax1 = plt.subplot2grid((2, 6), (0, 0), rowspan=1)
 
-        pos=np.arange(len(data))
+        pos = np.arange(len(data))
         ax1.set_yticks(pos)
         ax1.set_yticklabels(labels)
         ax1.invert_yaxis()
 
         ax1.set_xlabel('Hashtag Count')
         ax1.set_title('Cluster')
-        ax1.barh(pos,data)
+        ax1.barh(pos, data)
 
-    def piechart(self, labels=SAMPLE_LABELS_PIE,data=SAMPLE_DATA_PIE):
-        ax2 = plt.subplot2grid((2,6), (0,1), colspan=2)
+    def piechart(self, labels=SAMPLE_LABELS_PIE, data=SAMPLE_DATA_PIE):
+        ax2 = plt.subplot2grid((2, 6), (0, 1), colspan=2)
 
         ax2.pie(data, labels=labels, autopct='%1.2f%%', shadow=True, startangle=90)
         ax2.set_title('Origin')
         ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
     def scatterplot(self, labels=SAMPLE_LABELS_SCATTER, data=SAMPLE_DATA_SCATTER):
-        ax3 = plt.subplot2grid((2,6), (1,0), colspan=3)
+        ax3 = plt.subplot2grid((2, 6), (1, 0), colspan=3)
 
         ax3.xaxis.set_major_formatter(ticker.NullFormatter())
         ax3.xaxis.set_minor_formatter(ticker.FixedFormatter(labels))
@@ -49,23 +50,23 @@ class Visual(object):
         ticks = []
         pos = np.arange(len(data))
         for i in range(len(labels)):
-            tmp = int((i+0.5)*len(data)//len(labels))
+            tmp = int((i + 0.5) * len(data) // len(labels))
             ticks.append(pos[tmp])
 
-        ax3.xaxis.set_major_locator(ticker.IndexLocator(24,0))
+        ax3.xaxis.set_major_locator(ticker.IndexLocator(24, 0))
         ax3.xaxis.set_minor_locator(ticker.FixedLocator(ticks))
-        
+
         for tick in ax3.xaxis.get_minor_ticks():
             tick.tick1line.set_markersize(0)
 
         ax3.set_ylabel('Hashtag Count')
         ax3.set_title('Performance')
-        ax3.scatter(pos,data)
+        ax3.scatter(pos, data)
 
     def globalscatter(self, geoData=SAMPLE_GEO_DATA):
         loadList = ['america.csv', 'australia.csv', 'africa.csv', 'antarctica.csv', 'greenland.csv', 'europe_asia.csv']
 
-        ax4 = plt.subplot2grid((2,6), (0,3), colspan=3, rowspan=2, projection='3d')
+        ax4 = plt.subplot2grid((2, 6), (0, 3), colspan=3, rowspan=2, projection='3d')
 
         for i in range(len(loadList)):
             data = np.genfromtxt(loadList[i], delimiter=',')
@@ -81,9 +82,10 @@ class Visual(object):
         ax4.set_aspect('equal')
         ax4.axis('off')
 
+
 def dataconverter(data):
-    an = np.cos(data[:,0]*(np.pi/180))
-    x_array = np.cos(data[:,1]*(np.pi/180))*an
-    y_array = np.sin(data[:,1]*(np.pi/180))*an
-    z_array = np.sin(data[:,0]*(np.pi/180))
+    an = np.cos(data[:, 0] * (np.pi / 180))
+    x_array = np.cos(data[:, 1] * (np.pi / 180)) * an
+    y_array = np.sin(data[:, 1] * (np.pi / 180)) * an
+    z_array = np.sin(data[:, 0] * (np.pi / 180))
     return x_array, y_array, z_array
